c74dcd9c926e4c2d2ed9bbd178666b0d4ec5a7d1
Shine Selorm <selomshine1@gmail.com>
2025-09-11 15:08:56 +0000

    Add DetailView import and URL routing for views

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('', include('relationship_app.urls')),  # add this line
 ]
+
 
 
