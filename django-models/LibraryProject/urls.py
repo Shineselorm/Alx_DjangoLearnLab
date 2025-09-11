240f7e24e6571a162977683e3606cedae6b991e4
Shine Selorm <selomshine1@gmail.com>
2025-09-11 16:30:17 +0000

    Fix urls.py to use views.register explicitly

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -14,12 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # add this line
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Register
+    path('register/', views.register, name='register'),
+
+    # Built-in LoginView
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # Built-in LogoutView
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
 
