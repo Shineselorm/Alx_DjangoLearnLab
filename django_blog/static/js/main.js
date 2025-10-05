2f99d294be13c45f68f5a26df62351172e7d49db
Shine Selorm <selomshine1@gmail.com>
2025-10-05 16:49:50 +0000

    feat(blog): initialize Django project and blog app; add Post model, templates, static; configure settings; run initial migrations

---
+++ django_blog/static/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Simple placeholder script for future enhancements
+  // eslint-disable-next-line no-console
+  console.log('Django Blog assets loaded');
+});
+
+
