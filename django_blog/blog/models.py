d9c5c3e336062bfc8bf2e8e8b645ac0c138112d9
Shine Selorm <selomshine1@gmail.com>
2025-10-05 16:51:55 +0000

    chore(blog): use explicit User import for author FK per checker

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self) -> str:
         return self.title
